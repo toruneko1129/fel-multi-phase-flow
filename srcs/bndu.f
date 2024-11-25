0682de2e2a2de827b9ad6c491fc3f8c460babca2
toruneko1129 <kawakita.hayato@gmail.com>
2024-11-26 02:00:23 +0900

    updated bndu

--- srcs/bndu.f
+++ srcs/bndu.f
@@ -1,7 +1,7 @@
 ccc
 ccc<impose boundary conditions on the velocity components uk,vk,wk
 ccc
-      subroutine bndu(nID,ni,nj,nk,uk,vk,wk,uwall,yl,dy,l1)
+      subroutine bndu(nID,ni,nj,nk,uk,vk,wk,uwall,dy,l1)
 
       implicit none
       include 'param.h'
@@ -10,8 +10,8 @@ ccc
       real*8    uk(-2:ni+3,-2:nj+3,-2:nk+3)
       real*8    vk(-2:ni+3,-2:nj+3,-2:nk+3)
       real*8    wk(-2:ni+3,-2:nj+3,-2:nk+3)
-      real*8    uwall,yl,dy,l1
-      real*8    u1, u2, u3
+      real*8    uwall,dy,l1
+      real*8    coef1, coef2
 
       integer i,j,k
 
@@ -19,9 +19,8 @@ ccc
 ccc<j
 ccc
 ccc
-      u1 = (yl+dy  )/(yl+l1*2)*uwall
-      u2 = (yl+dy*3)/(yl+l1*2)*uwall
-      u3 = (yl+dy*5)/(yl+l1*2)*uwall
+      coef1 = (2.d0 * dy) / (2.d0 * l1 + dy)
+      coef2 = (2.d0 * l1 - dy) / (2.d0 * l1 + dy)
 
       if(nID(Y_MINUS).lt.0)then
 !$OMP  PARALLEL DO
@@ -29,20 +28,20 @@ ccc
 !$OMP$ DEFAULT(none)
 !$OMP$ PRIVATE(i,k)
 !$OMP$ SHARED(ni,nk)
-!$OMP$ SHARED(uk,vk,wk,u1,u2,u3)
+!$OMP$ SHARED(uk,vk,wk,uwall,dy,l1,coef1,coef2)
       do k=-2,nk+3
       do i=-2,ni+3
-      uk(i,  -2,k)=-u3
-      uk(i,  -1,k)=-u2
-      uk(i,   0,k)=-u1
+      uk(i,   0,k) = coef1 * (-uwall) + coef2 * uk(i,   1,k)
+      uk(i,  -1,k) = 2.d0 * uk(i,   0,k) - uk(i,   1,k)
+      uk(i,  -2,k) = 2.d0 * uk(i,  -1,k) - uk(i,   0,k)
 
-      vk(i,  -2,k)=vk(i,   2,k)
-      vk(i,  -1,k)=vk(i,   1,k)
+      vk(i,  -2,k)=-vk(i,   2,k)
+      vk(i,  -1,k)=-vk(i,   1,k)
       vk(i,   0,k)=0.0d0
 
-      wk(i,  -2,k)=wk(i,   3,k)
-      wk(i,  -1,k)=wk(i,   2,k)
-      wk(i,   0,k)=wk(i,   1,k)
+      wk(i,  -2,k)=-wk(i,   3,k)
+      wk(i,  -1,k)=-wk(i,   2,k)
+      wk(i,   0,k)=-wk(i,   1,k)
       enddo
       enddo
 !$OMP  END PARALLEL DO
@@ -54,21 +53,21 @@ ccc
 !$OMP$ DEFAULT(none)
 !$OMP$ PRIVATE(i,k)
 !$OMP$ SHARED(ni,nj,nk)
-!$OMP$ SHARED(uk,vk,wk,u1,u2,u3)
+!$OMP$ SHARED(uk,vk,wk,uwall,dy,l1,coef1,coef2)
       do k=-2,nk+3
       do i=-2,ni+3
-      uk(i,nj+1,k)=u1
-      uk(i,nj+2,k)=u2
-      uk(i,nj+3,k)=u3
+      uk(i,nj+1,k) = coef1 * uwall + coef2 * uk(i,nj  ,k)
+      uk(i,nj+2,k) = 2.d0 * uk(i,nj+1,k) - uk(i,nj  ,k)
+      uk(i,nj+3,k) = 2.d0 * uk(i,nj+2,k) - uk(i,nj+1,k)
 
       vk(i,nj  ,k)=0.0d0
-      vk(i,nj+1,k)=vk(i,nj-1,k)
-      vk(i,nj+2,k)=vk(i,nj-2,k)
-      vk(i,nj+3,k)=vk(i,nj-3,k)
+      vk(i,nj+1,k)=-vk(i,nj-1,k)
+      vk(i,nj+2,k)=-vk(i,nj-2,k)
+      vk(i,nj+3,k)=-vk(i,nj-3,k)
 
-      wk(i,nj+1,k)=wk(i,nj  ,k)
-      wk(i,nj+2,k)=wk(i,nj-1,k)
-      wk(i,nj+3,k)=wk(i,nj-2,k)
+      wk(i,nj+1,k)=-wk(i,nj  ,k)
+      wk(i,nj+2,k)=-wk(i,nj-1,k)
+      wk(i,nj+3,k)=-wk(i,nj-2,k)
       enddo
       enddo
 !$OMP  END PARALLEL DO
