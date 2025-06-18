6a8e336df58937bdc5e7ec46725a8e9cf99afd1b
toruneko1129 <kawakita.hayato@gmail.com>
2025-06-18 13:14:50 +0900

    add: pattern ratio process

--- srcs/init_array_pt_ratio.f90
+++ srcs/init_array_pt_ratio.f90
@@ -1,9 +1,9 @@
-subroutine init_array(ni,nj,nk,q_a,q_b,q_array,width)
+subroutine init_array_pt_ratio(ni,nj,nk,q_a,q_b,q_array,period,ratio_a)
   implicit none
   integer :: ni,nj,nk
   real*8 :: q_a, q_b
   real*8 :: q_array(-2:ni+3,-2:nj+3,-2:nk+3)
-  integer :: width, period
+  integer :: period, ratio_a
   integer :: i,j,k
 
   !$OMP PARALLEL DO DEFAULT(NONE) PRIVATE(i,j,k) & 
@@ -17,21 +17,22 @@ subroutine init_array(ni,nj,nk,q_a,q_b,q_array,width)
   end do
   !$OMP END PARALLEL DO
 
-  period = width * 2
-
   !$OMP PARALLEL DO DEFAULT(NONE) PRIVATE(i,k) & 
-  !$OMP& SHARED(ni,nj,nk,q_a,q_b,q_array, period, width) SCHEDULE(static,1)
+  !$OMP& SHARED(ni,nj,nk,q_a,q_b,q_array, period, ratio_a) SCHEDULE(static,1)
   do k = -2, nk+3
     do i = -2, ni+3
       q_array(i, 1, k) = q_a
-      if ( mod(i+period*2-1, period) < width) then
+      if ( mod(i+period*2-1, period) < ratio_a) then
         q_array(i, nj, k) = q_a
       else
         q_array(i, nj, k) = q_b
       endif
+      if ((k == 0) .AND. (0 < i) .AND. (i <= 16) ) then
+        write(*,'(20e20.10)') q_array(i,nj,k)
+      end if
     end do
   end do
   !$OMP END PARALLEL DO
 
   return
-end subroutine init_array
+end subroutine init_array_pt_ratio
